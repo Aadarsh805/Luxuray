d25d4dab8bff965a74a9b8b3098af67984007c48
aadarsh805 <aadarshthakur80501@gmail.com>
2022-12-14 01:07:23 +0530

    initialized repo

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="font-8xl text-red-500 font-bold">APPPP</h1>
+    </div>
+  );
+};
+
+export default HomePage;
